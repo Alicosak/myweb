2d5950e7738f112428dd0f81839b2c1aafbc2760
Nicolas E. Pinto Machauay <80487640+Alicosak@users.noreply.github.com>
2023-04-22 20:55:33 -0500

    cambiado los colores y agregado el Typed
    Se cambiaron los colores (var)
    creado Typed
    Para iniciar About

--- js/script.js
+++ js/script.js
@@ -8,4 +8,12 @@ MenuBtn.addEventListener("click", function(e) {
     this.classList.toggle('fa-xmark')
 })
 
+//Typing Effect
 
+let typed = new Typed('.auto-input',{
+    strings:[' Systems Analyst',' Support Technician',' IT Systems Operator',' Front-End Developer'],
+    typeSpeed:80,
+    backSpeed:80,
+    backDelay:2000,
+    loop:true,
+})
